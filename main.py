dd0d7cab60259d3610f5dac428b0f3899157a53a
Anastasia <ashevchenko629@gmail.com>
2024-01-08 03:03:59 +0100

    not dead yet

--- main.py
+++ main.py
@@ -25,39 +25,3 @@ app.include_router(contacts.router, prefix="/api")
 def index():
     return {"message": "Contact App"}
 
-
-@app.get("/api/healthchecker")
-async def healthchecker(db: AsyncSession = Depends(get_db)):
-    try:
-        # Make request
-        result = await db.execute(text("SELECT 1"))
-        result = result.fetchone()
-        if result is None:
-            raise HTTPException(
-                status_code=500, detail="Database is not configured correctly"
-            )
-        return {"message": "Welcome to FastAPI!"}
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=500, detail="Error connecting to the database")
-
-@app.get("/api/test")
-async def test(db: AsyncSession = Depends(get_db)):
-
-    async with db.begin() as conn:
-        row = await db.execute(text("SELECT current_database() AS database_name"))
-
-    database_name = row.scalar() if row else None
-    print(database_name)
-    # try:
-    #     # Make request
-    #     result = await db.execute(text("SELECT * FROM users"))
-    #     result = result.fetchone()
-    #     if result is None:
-    #         raise HTTPException(
-    #             status_code=500, detail="Database is not configured correctly"
-    #         )
-    #     return {"message": "Welcome to FastAPI!"}
-    # except Exception as e:
-    #     print(e)
-    #     raise HTTPException(status_code=500, detail="Error connecting to the database")
